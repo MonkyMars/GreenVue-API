6458f511b4b57c06f28506ca04dfaf5d6668f849
Levi Noppers <levinoppers69@gmail.com>
2025-04-05 11:55:24 +0200

    feat: update CORS configuration to allow all origins and disable credentials

--- internal/api/router.go
+++ internal/api/router.go
@@ -54,10 +54,10 @@ func setupMiddleware(app *fiber.App) {
 	}))
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000, http://localhost:8082",
+		AllowOrigins:     "*",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS,PATCH",
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
-		AllowCredentials: true,
+		AllowCredentials: false,
 	}))
 
 	// Configure custom rate limiter with different limits for different endpoints
