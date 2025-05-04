75fbdb754fe28638a18dd96a0671fb7e1a6bb829
Levi Noppers <levinoppers69@gmail.com>
2025-05-05 00:15:48 +0200

    fix: update CORS AllowOrigins to include the new domain for improved access control

--- internal/api/router.go
+++ internal/api/router.go
@@ -58,7 +58,7 @@ func setupMiddleware(app *fiber.App) {
 	}))
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://192.168.178.10,http://localhost:3000,http://localhost:8081,http://10.0.2.2:3000,https://greentradeeu.vercel.app,https://greentrade.site",
+		AllowOrigins:     "http://192.168.178.10,http://localhost:3000,http://localhost:8081,http://10.0.2.2:3000,https://greentradeeu.vercel.app,https://www.greentrade.site",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS,PATCH",
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 		AllowCredentials: false,
